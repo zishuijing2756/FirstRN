d07feeb51e27d8faad8b9b44f02ed73b6310fa3c
yangnana <yangnana2756@gmail.com>
2019-06-18 16:39:31 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+// import ButtonBasics from  './ButtonBasics';
+import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName,()=>ButtonBasics);
+AppRegistry.registerComponent(appName, () => App);
